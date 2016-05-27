e03cb5963a380b3313363b83894da75971873b66
j4v <xavier.garceau-aranda@polymtl.ca>
2016-05-27 19:18:06 +0200

    Include spyder in apps

--- cmspyder/cmspyder/settings.py
+++ cmspyder/cmspyder/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'djcelery',
+]
+
+LOCAL_APPS = [
+    'spyder',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
