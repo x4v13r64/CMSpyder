9cca2b44706cf26025f14d57536af3fffaaa17be
j4v <xavier.garceau-aranda@polymtl.ca>
2016-05-27 19:18:16 +0200

    Initial admin

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Domain
+from tasks import discover_type
+
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ['domain', 'type', 'last_crawl']
+    ordering = ['domain']
+    actions = ['discover_type']
+
+    def discover_type(self, request, queryset):
+        for domain in queryset:
+            discover_type.delay(domain.id)
+        self.message_user(request, 'Task(s) created')
+    discover_type.short_description = 'Discover the type of the blog(s)'
+
+admin.site.register(Domain, DomainAdmin)
