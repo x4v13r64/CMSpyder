2571705412834fa42242155f0456aa2fc9e3c995
j4v <xavier.garceau-aranda@polymtl.ca>
2016-05-27 17:50:46 +0200

    Initial models

--- cmspyder/spyder/models.py
+++ cmspyder/spyder/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Domain(models.Model):
+    domain = models.CharField()
+    BLOG_TYPES = (
+        ('none', 'Unknonw'),
+        ('wordpress', 'WordPress'),
+    )
+    type = models.CharField(max_length=50, choices=BLOG_TYPES, default='none', null=True)
+    last_crawl = models.DateTimeField('Last crawl', blank=True, null=True)
