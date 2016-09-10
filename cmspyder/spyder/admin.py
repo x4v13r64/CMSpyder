7b7ce3fd3afc941b27785e874f3de312f3e3d093
j4v <xavier.garceau-aranda@polymtl.ca>
2016-09-10 17:55:15 +0200

    Update admin

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -1,19 +1,42 @@
 from django.contrib import admin
 
-from models import Domain
+from models import TLD, Domain, Subdomain, ScanResult, PluginResult
 from tasks import discover_type
 
 
+class TLDAdmin(admin.ModelAdmin):
+    model = TLD
+admin.site.register(TLD, TLDAdmin)
+
+
 class DomainAdmin(admin.ModelAdmin):
-    list_display = ['domain', 'type', 'last_crawl']
-    list_filter = ['type']
-    actions = ['discover_type']
-    ordering = ['domain']
-
-    def discover_type(self, request, queryset):
-        for domain in queryset:
-            discover_type.delay(domain.id)
-        self.message_user(request, 'Task(s) created')
-    discover_type.short_description = 'Discover the type of the blog(s)'
-
+    model = Domain
+    list_display = ['domain', 'tld']
+    list_filter = ['tld']
 admin.site.register(Domain, DomainAdmin)
+
+class SubdomainAdmin(admin.ModelAdmin):
+    model = Domain
+    list_display = ['subdomain', 'domain', 'get_domain_tld']
+    list_filter = ['domain', 'domain__tld']
+
+    def get_domain_tld(self, obj):
+        return obj.domain.tld
+    get_domain_tld.short_description = 'TLD'
+    get_domain_tld.admin_order_field = 'domain__tld'
+
+#     actions = ['discover_type']
+#     ordering = ['domain']
+#
+#     def discover_type(self, request, queryset):
+#         for domain in queryset:
+#             discover_type.delay(domain.id)
+#         self.message_user(request, 'Task(s) created')
+#     discover_type.short_description = 'Discover the type of the blog(s)'
+#
+admin.site.register(Subdomain, SubdomainAdmin)
+
+
+# class ScanResultAdmin(admin.ModelAdmin):
+#     model = ScanResult
+# admin.site.register(ScanResult, ScanResultAdmin)
