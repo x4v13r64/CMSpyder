304109bdcb2e317bdf232799fd5ab5b02739eeb3
j4v <xavier.garceau-aranda@polymtl.ca>
2016-09-10 17:59:11 +0200

    Update admin

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -5,20 +5,21 @@ from tasks import discover_type
 
 
 class TLDAdmin(admin.ModelAdmin):
-    model = TLD
+    search_fields = ['tld']
 admin.site.register(TLD, TLDAdmin)
 
 
 class DomainAdmin(admin.ModelAdmin):
-    model = Domain
     list_display = ['domain', 'tld']
     list_filter = ['tld']
+    search_fields = ['domain', 'tld']
 admin.site.register(Domain, DomainAdmin)
 
+
 class SubdomainAdmin(admin.ModelAdmin):
-    model = Domain
     list_display = ['subdomain', 'domain', 'get_domain_tld']
     list_filter = ['domain', 'domain__tld']
+    search_fields = ['subdomain']
 
     def get_domain_tld(self, obj):
         return obj.domain.tld
@@ -26,17 +27,11 @@ class SubdomainAdmin(admin.ModelAdmin):
     get_domain_tld.admin_order_field = 'domain__tld'
 
 #     actions = ['discover_type']
-#     ordering = ['domain']
 #
 #     def discover_type(self, request, queryset):
 #         for domain in queryset:
 #             discover_type.delay(domain.id)
 #         self.message_user(request, 'Task(s) created')
 #     discover_type.short_description = 'Discover the type of the blog(s)'
-#
+
 admin.site.register(Subdomain, SubdomainAdmin)
-
-
-# class ScanResultAdmin(admin.ModelAdmin):
-#     model = ScanResult
-# admin.site.register(ScanResult, ScanResultAdmin)
