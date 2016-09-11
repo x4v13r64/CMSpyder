b535dd4d5c195a51e3e16a0fb76f6c2db958d1d3
j4v <xavier.garceau-aranda@polymtl.ca>
2016-09-11 19:31:22 +0200

    Model & admin refactoring

--- cmspyder/domains/models.py
+++ cmspyder/domains/models.py
@@ -34,7 +34,8 @@ class Subdomain(models.Model):
     subdomain = models.CharField(max_length=250, blank=True)
 
     def __unicode__(self):
-        # print self.domain
+        return u"{}".format(self.subdomain)
+        # TODO why does this make ScanError admin lag
         if self.subdomain:
             return u"{}.{}".format(self.subdomain, self.domain)
         else:
--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -4,13 +4,13 @@ from models import PluginResult, ScanResult, ScanError
 
 
 class ScanResultAdmin(admin.ModelAdmin):
-    list_display = ['id']
     list_display = ['subdomain', 'created', 'type', 'version']
-    list_filter = ['subdomain__subdomain', 'type']
+    list_filter = ['type']
+    search_fields = ['subdomain__subdomain']
 admin.site.register(ScanResult, ScanResultAdmin)
 
 
 class ScanErrorAdmin(admin.ModelAdmin):
-    list_display = ['subdomain', 'created']
-    list_filter = ['subdomain__subdomain']
+    list_display = ['created', 'subdomain']
+    search_fields = ['subdomain__subdomain']
 admin.site.register(ScanError, ScanErrorAdmin)
