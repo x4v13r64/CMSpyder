82bb92c4f2861b594e978f24cc9eca29b8ec0aa2
j4v <xavier.garceau-aranda@polymtl.ca>
2016-09-11 19:08:39 +0200

    Update spyder admin

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -4,12 +4,13 @@ from models import PluginResult, ScanResult, ScanError
 
 
 class ScanResultAdmin(admin.ModelAdmin):
+    list_display = ['id']
     list_display = ['subdomain', 'created', 'type', 'version']
-    list_filter = ['subdomain', 'type']
+    list_filter = ['subdomain__subdomain', 'type']
 admin.site.register(ScanResult, ScanResultAdmin)
 
 
 class ScanErrorAdmin(admin.ModelAdmin):
     list_display = ['subdomain', 'created']
-    list_filter = ['subdomain']
+    list_filter = ['subdomain__subdomain']
 admin.site.register(ScanError, ScanErrorAdmin)
