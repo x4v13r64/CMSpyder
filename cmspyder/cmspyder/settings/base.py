7ab2af76145ffd55d6f14f039a511c35ccddc341
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-09-25 11:38:20 +0200

    Set dev db to proper directory

--- cmspyder/cmspyder/settings/base.py
+++ cmspyder/cmspyder/settings/base.py
@@ -1,7 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Application definition
 DJANGO_APPS = [
