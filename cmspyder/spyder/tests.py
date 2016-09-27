a88bd215e7be94b5e9586930ee16d3596dff6d31
Xavier Garceau-Aranda <xavier.garceau-aranda@polymtl.ca>
2016-09-27 10:00:16 +0200

    Add initial test case

--- cmspyder/spyder/tests.py
+++ cmspyder/spyder/tests.py
@@ -1,3 +1,16 @@
+from spyder.tasks import detect_cms
+from domains.models import *
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class DetectCMSTestCase(TestCase):
+    def setUp(self):
+        com_tld = TLD.objects.create(tld='com')
+        google_domain = Domain.objects.create(tld=com_tld,
+                                              domain='google')
+        google_subdomain = Subdomain(domain=google_domain)
+
+    def test_detect_cms(self):
+        google_subdomain = Subdomain.objects.get(domain='google')
+        detect_cms(google_subdomain.id)
