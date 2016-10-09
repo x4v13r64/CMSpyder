c2bf2d814b47b16e5032b3e50c5f6347b484f722
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-10-09 09:42:32 +0200

    Make scan result and error results admin read-only

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -7,10 +7,12 @@ class ScanResultAdmin(admin.ModelAdmin):
     list_display = ['id', 'created', 'subdomain', 'type', 'version']
     list_filter = ['type']
     search_fields = ['subdomain__subdomain', 'subdomain__domain__domain']
+    readonly_fields = ['subdomain', 'type', 'version']
 admin.site.register(ScanResult, ScanResultAdmin)
 
 
 class ScanErrorAdmin(admin.ModelAdmin):
     list_display = ['id', 'created', 'subdomain']
     search_fields = ['subdomain__subdomain', 'subdomain__domain__domain']
+    readonly_fields = ['subdomain', 'error']
 admin.site.register(ScanError, ScanErrorAdmin)
