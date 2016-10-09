d98ddcd3411dc0f0c9c1bd94f34a5925085250a8
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-10-09 09:26:38 +0200

    Improve spyder admin

--- cmspyder/spyder/admin.py
+++ cmspyder/spyder/admin.py
@@ -4,13 +4,13 @@ from models import PluginResult, ScanResult, ScanError
 
 
 class ScanResultAdmin(admin.ModelAdmin):
-    list_display = ['id', 'subdomain', 'created', 'type', 'version']
+    list_display = ['id', 'created', 'subdomain', 'type', 'version']
     list_filter = ['type']
-    search_fields = ['subdomain__subdomain']
+    search_fields = ['subdomain__subdomain', 'subdomain__domain__domain']
 admin.site.register(ScanResult, ScanResultAdmin)
 
 
 class ScanErrorAdmin(admin.ModelAdmin):
     list_display = ['id', 'created', 'subdomain']
-    search_fields = ['subdomain__subdomain']
+    search_fields = ['subdomain__subdomain', 'subdomain__domain__domain']
 admin.site.register(ScanError, ScanErrorAdmin)
