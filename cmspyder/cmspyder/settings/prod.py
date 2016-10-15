1c5ae90767228d65730fc315654ebc84b97f82a4
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-10-15 10:31:20 +0200

    Split celery queues for each task

--- cmspyder/cmspyder/settings/prod.py
+++ cmspyder/cmspyder/settings/prod.py
@@ -27,13 +27,22 @@ BROKER_URL = 'amqp://%s:%s@%s:%s//' % (os.environ['RABBIT_MQ_USER'],
                                        os.environ['RABBIT_MQ_HOST'],
                                        os.environ['RABBIT_MQ_PORT'],)
 
-CELERY_DEFAULT_QUEUE = 'cmspyderqueue'
+CELERY_DEFAULT_QUEUE = 'celery_default_queue'
+CMSPYDER_DISCOVER_DOMAINS_QUEUE = 'cmspyder_discover_doamins_queue'
+CMSPYDER_DETECT_CMS_QUEUE = 'cmspyder_detect_cms_queue'
 CELERY_QUEUES = {
+    CMSPYDER_DISCOVER_DOMAINS_QUEUE: {
+        'exchange': CMSPYDER_DISCOVER_DOMAINS_QUEUE,
+        'binding_key': CMSPYDER_DISCOVER_DOMAINS_QUEUE,
+    },
     CELERY_DEFAULT_QUEUE: {
         'exchange': CELERY_DEFAULT_QUEUE,
         'binding_key': CELERY_DEFAULT_QUEUE,
     }
 }
+CELERY_ROUTES = {'spyder.tasks.discover_domains': {'queue': CMSPYDER_DISCOVER_DOMAINS_QUEUE},
+                 'spyder.tasks.detect_cms': {'queue': CMSPYDER_DETECT_CMS_QUEUE}}
+
 CELERY_IGNORE_RESULT = True
 # CELERY_RESULT_BACKEND = 'amqp'
 
