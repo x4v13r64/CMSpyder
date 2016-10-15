c47bcc0b56ad15bb2aff04a2b8d7bab93765e210
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-10-15 09:55:48 +0200

    Set locale

--- cmspyder/cmspyder/settings/base.py
+++ cmspyder/cmspyder/settings/base.py
@@ -73,11 +73,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
+if 'TIME_ZONE' in os.environ:
+    TIME_ZONE = os.environ['TIME_ZONE']
+else:
+    TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
