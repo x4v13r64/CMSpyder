f257f3a8b45ab0a814cf58a6c0a88b5421ad19b6
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-10-22 13:53:16 +0200

    Optimize db queries

--- cmspyder/spyder/management/commands/regenerate_site.py
+++ cmspyder/spyder/management/commands/regenerate_site.py
@@ -23,42 +23,36 @@ class Command(BaseCommand):
 
             index.write('<h2>General statistics</h2>')
             index.write('<ul>')
-            subdomains = Subdomain.objects.filter()
-            index.write('<li>Domain count: %s</li>' % len(subdomains))
+            index.write('<li>Domain count: %s</li>' % Subdomain.objects.count())
 
             scan_results = ScanResult.objects.filter()
-            scan_results_unique_subdomain = scan_results.values('subdomain').distinct()
-            index.write('<li>Unique domains analyzed: %s</li>' % len(scan_results_unique_subdomain))
-            index.write('<li>Analysis count: %s</li>' % len(scan_results))
+            index.write('<li>Unique domains analyzed: %s</li>' %
+                        scan_results.values('subdomain').distinct().count())
+            index.write('<li>Analysis count: %s</li>' % ScanResult.objects.count())
             index.write('</ul>')
 
             index.write('<h2>CMS detection results</h2>')
-            unique_scan_results = scan_results.values('type').distinct()
-
             # TODO these stats are incorrect as they aren't for unique detections
-            for result_type in unique_scan_results:
+            for result_type in scan_results.values('type').distinct():
                 index.write('<h3>%s</h3>' % result_type['type'])
 
-                scan_results_for_type = scan_results.filter(type=result_type['type'])
+                scan_results_for_type = ScanResult.objects.filter(type=result_type['type'])
 
                 index.write('<ul>')
                 index.write('<li>total count: %s (%s%% of CMS detections)</li>' %
-                            (len(scan_results_for_type),
-                             len(scan_results_for_type)*100/len(scan_results)))
+                            (ScanResult.objects.filter(type=result_type['type']).count(),
+                             ScanResult.objects.filter(type=result_type['type']).count() *
+                             100/ScanResult.objects.count()))
 
-                scan_results_versions_for_type = \
-                    scan_results_for_type.values('version').distinct()
-
-                for version in scan_results_versions_for_type:
+                for version in scan_results_for_type.values('version').distinct():
                     index.write('<ul>')
-                    scan_results_versions_for_type_version = \
-                        ScanResult.objects.filter(type=result_type['type'],
-                                                  version=version['version'])
                     index.write('<li>version \'%s\' count: %s (%s%% of %s detections)</li>' %
                                 (version['version'] if version['version'] else 'unknown',
-                                 len(scan_results_versions_for_type_version),
-                                 len(scan_results_versions_for_type_version) *
-                                 100/len(scan_results_for_type),
+                                 ScanResult.objects.filter(type=result_type['type'],
+                                                           version=version['version']).count(),
+                                 ScanResult.objects.filter(type=result_type['type'],
+                                                           version=version['version']).count() *
+                                 100/ScanResult.objects.filter(type=result_type['type']).count(),
                                  result_type['type']))
                     index.write('</ul>')
 
