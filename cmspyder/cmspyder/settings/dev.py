de2797bb9ec12ff486536607d0c325bd579f0afb
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-11-07 22:17:11 +0100

    Update xmlrunner config

--- cmspyder/cmspyder/settings/dev.py
+++ cmspyder/cmspyder/settings/dev.py
@@ -39,10 +39,8 @@ DATABASES = {
     }
 }
 
-# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-# TEST_OUTPUT_DIR = 'xmlrunner'
+TEST_OUTPUT_DIR = os.path.dirname(BASE_DIR)
 TEST_OUTPUT_VERBOSE = True
 TEST_OUTPUT_DESCRIPTIONS = True
 
