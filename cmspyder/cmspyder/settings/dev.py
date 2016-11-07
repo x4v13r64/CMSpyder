04122381570a5b1b1df892ec11194d605dc0a193
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-11-07 22:05:56 +0100

    Add unittest-xml-reporting config to dev

--- cmspyder/cmspyder/settings/dev.py
+++ cmspyder/cmspyder/settings/dev.py
@@ -39,7 +39,12 @@ DATABASES = {
     }
 }
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.run_tests'
+TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_DIR = 'xmlrunner'
 
 # celery config
 BROKER_URL = 'django://'
