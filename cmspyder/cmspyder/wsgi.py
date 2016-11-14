05cb8288da3609cbcf783cb5ad01c213a1953fe6
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-11-14 22:35:00 +0100

    Initial gunicorn configuration

--- cmspyder/cmspyder/wsgi.py
+++ cmspyder/cmspyder/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmspyder.settings")
+# todo use prod settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmspyder.settings.dev")
 
 application = get_wsgi_application()
---
+++ cmspyder/start_gunicorn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn cmspyder.wsgi:application --bind 0.0.0.0:8000 --workers 3
+
