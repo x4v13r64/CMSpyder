511c84cbeb2fa7283d0a44f64e17701ebf6dc424
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-11-14 20:21:04 +0100

    Integrate version detection to python wappalyzer native plugin.

--- cmspyder/spyder/detection_plugins/wappalyzer.py
+++ cmspyder/spyder/detection_plugins/wappalyzer.py
@@ -1,6 +1,7 @@
 from spyder.detection_plugins.base import BasePlugin
 from spyder.drivers.wappalyzer import WappalyzerDriver
 from spyder.models import ScanResult
+from distutils.version import LooseVersion
 
 
 class WappalyzerPlugin(BasePlugin):
@@ -13,13 +14,25 @@ class WappalyzerPlugin(BasePlugin):
 
     def detect(self, subdomain, requests_results):
         if '/' in requests_results:
-            results = self.wappalyzer_driver.analyze('http://%s/' % subdomain,  # TODO fix this?
+            results = self.wappalyzer_driver.analyze('http://%s/' % subdomain,
                                                      dict(requests_results['/'].headers),
                                                      requests_results['/'].text)
 
             for r in results:
                 if 'CMS' in results[r]['categories']:
+
+                    # find greatest version
+                    if len(results[r]['versions']) > 1:
+                        greatest_version = results[r]['versions'][0]
+                        for v in results[r]['versions']:
+                            if LooseVersion(v) > LooseVersion(greatest_version):
+                                greatest_version = v
+                        version = 1
+                    elif len(results[r]['versions']) == 1:
+                        version = results[r]['versions'][0]
+                    else:
+                        version = None
+
                     ScanResult.objects.create(subdomain=subdomain,
                                               type=r.lower(),
-                                              version=results[r]['version'] if 'version' in
-                                              results[r] else '')
+                                              version=version if version else '')
--- cmspyder/spyder/drivers/wappalyzer.py
+++ cmspyder/spyder/drivers/wappalyzer.py
@@ -8,10 +8,54 @@ from django.conf import settings
 
 __credit__ = "https://github.com/scrapinghub/wappalyzer-python"
 
+
+class Pattern(object):
+    """
+    Each pattern in the apps.json file will be kept as an object, who stores the compiled regex
+    as well as if the regex returns a version.
+    """
+
+    def __init__(self, pattern):
+        """
+
+        :param pattern:
+        :param has_version:
+        """
+
+        regex, _, rest = pattern.partition('\\;')
+
+        try:
+            self.regex = re.compile(regex, re.I)
+            self.has_version = 'version' in rest  # todo improve version detection
+        except re.error as e:
+            warnings.warn("Caught '{error}' compiling regex: {regex}".format(error=e, regex=regex))
+            self.pattern = None
+
+
 class WappalyzerDriver(object):
     """
     Python Wappalyzer driver.
     """
+
+    # todo detect versions that are not of type \1:
+    """
+    version:\1
+    version:\1?\1:\2
+    version:\1 \2
+    version:\1?4.1+:
+    version:2
+    version:2
+    version:\1?Enterprise:Community
+    confidence:50\;version:2
+    version:\1.\2.\3
+    version:API v\1
+    version:\1?4:5
+    version:2+
+    version:\1?2+:
+    version:\1?\1:\2
+    """
+    # todo also detect "confidence"
+    # todo also regex 'env'
 
     def __init__(self):
         """
@@ -36,7 +80,7 @@ class WappalyzerDriver(object):
         :return:
         """
 
-        # Ensure these keys' values are lists
+        # Ensure keys' values are lists
         for key in ['url', 'html', 'script', 'implies']:
             try:
                 value = app[key]
@@ -46,14 +90,14 @@ class WappalyzerDriver(object):
                 if not isinstance(value, list):
                     app[key] = [value]
 
-        # Ensure these keys exist
+        # Ensure keys exist
         for key in ['headers', 'meta']:
             try:
                 value = app[key]
             except KeyError:
                 app[key] = {}
 
-        # Ensure the 'meta' key is a dict
+        # Ensure 'meta' key is a dict
         obj = app['meta']
         if not isinstance(obj, dict):
             app['meta'] = {'generator': obj}
@@ -65,28 +109,12 @@ class WappalyzerDriver(object):
 
         # Prepare regular expression patterns
         for key in ['url', 'html', 'script']:
-            app[key] = [self._prepare_pattern(pattern) for pattern in app[key]]
+            app[key] = [Pattern(pattern) for pattern in app[key]]
 
         for key in ['headers', 'meta']:
             obj = app[key]
             for name, pattern in obj.items():
-                obj[name] = self._prepare_pattern(obj[name])
-
-    def _prepare_pattern(self, pattern):
-        """
-        Strip out key:value pairs from the pattern and compile the regular
-        expression.
-        :param pattern:
-        :return:
-        """
-
-        regex, _, rest = pattern.partition('\\;')
-        try:
-            return re.compile(regex, re.I)
-        except re.error as e:
-            warnings.warn("Caught '{error}' compiling regex: {regex}".format(error=e, regex=regex))
-            # regex that never matches: http://stackoverflow.com/a/1845097/413622
-            return re.compile(r'(?!x)x')
+                obj[name] = Pattern(obj[name])
 
     def _has_app(self, app, url, headers, html, scripts, meta):
         """
@@ -100,36 +128,63 @@ class WappalyzerDriver(object):
         :return:
         """
 
-        # Search the easiest things first and save the full-text search of the HTML for last
-        for regex in app['url']:
-            if regex.search(url):
-                a = regex.search(url).group(0)
-                return True
+        found = False
+        versions = set()
 
-        for name, regex in app['headers'].items():
-            if name in headers:
-                content = headers[name]
-                if regex.search(content):
-                    a = regex.search(content).group(0)
-                    return True
+        for pattern in app['url']:
+            if pattern.regex and pattern.regex.search(url):
+                # detected
+                found = True
+                # find version
+                if pattern.has_version:
+                    versions.update(pattern.regex.search(url).group(1))
 
-        for regex in app['script']:
+        for name, pattern in app['headers'].items():
+            if name.lower() in headers:
+                content = headers[name.lower()]
+                if pattern.regex and pattern.regex.search(content):
+                    # detected
+                    found = True
+                    # find version
+                    if pattern.has_version:
+                        version = pattern.regex.search(content).group(1)
+                        if version:
+                            versions.update(version)
+
+        for pattern in app['script']:
             for script in scripts:
-                if regex.search(script):
-                    a = regex.search(script).group(0)
-                    return True
+                if pattern.regex and pattern.regex.search(script):
+                    # detected
+                    found = True
+                    # find version
+                    if pattern.has_version:
+                        version = pattern.regex.search(script).group(1)
+                        if version:
+                            versions.update(version)
 
-        for name, regex in app['meta'].items():
-            if name in meta:
+        for name, pattern in app['meta'].items():
+            if name.lower() in meta:
                 content = meta[name]
-                if regex.search(content):
-                    a = regex.search(content).group(0)
-                    a = regex.search(content).group(1)
-                    return True
+                if pattern.regex and pattern.regex.search(content):
+                    # detected
+                    found = True
+                    # find version
+                    if pattern.has_version:
+                        version = pattern.regex.search(content).group(1)
+                        if version:
+                            versions.update(version)
 
-        for regex in app['html']:
-            if regex.search(html):
-                return True
+        for pattern in app['html']:
+            if pattern.regex and pattern.regex.search(html):
+                # detected
+                found = True
+                # find version
+                if pattern.has_version:
+                    version = pattern.regex.search(html).group(1)
+                    if version:
+                        versions.update(version)
+
+        return found, versions
 
     def _get_implied_apps(self, detected_apps):
         """
@@ -184,19 +239,28 @@ class WappalyzerDriver(object):
         scripts = [script['src'] for script in parsed_html.findAll('script', src=True)]
         meta = {meta['name'].lower(): meta['content'] for meta in
                 parsed_html.findAll('meta', attrs=dict(name=True, content=True))}
+        # make all headers lowercase
+        headers_lower = {h.lower(): v for h, v in headers.items()}
 
-        detected_apps = set()
+        detected_apps = dict()
 
         for app_name, app in self.apps.items():
-            if self._has_app(app, url, headers, html, scripts, meta):
-                detected_apps.add(app_name)
+            has_app, versions = self._has_app(app, url, headers_lower, html, scripts, meta)
+            if has_app:
+                if app_name in detected_apps:
+                    detected_apps[app_name] |= versions
+                else:
+                    detected_apps[app_name] = versions
 
-        detected_apps |= self._get_implied_apps(detected_apps)
+        # todo support the get_implied_apps method
+        # dict not compatible with set
+        # detected_apps |= self._get_implied_apps(detected_apps)  # merge set
 
         categorised_apps = {}
 
         for app_name in detected_apps:
             cat_names = self.get_categories(app_name)
-            categorised_apps[app_name] = {"categories": cat_names}
+            categorised_apps[app_name] = {"categories": cat_names,
+                                          "versions": list(detected_apps[app_name])}
 
         return categorised_apps
