4eeca20358ca3a6d5542b9be5a58d2f9471c81a2
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2016-12-06 11:37:07 +0100

    Add custom admin site header.

--- cmspyder/cmspyder/urls.py
+++ cmspyder/cmspyder/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+admin.site.site_header = 'CMSpyder Administration'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
