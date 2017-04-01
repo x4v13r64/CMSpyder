e1d39ebc8794f21c4d807337539421e16551e692
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2017-04-01 17:38:50 +0200

    Restore rabbitmq code

--- cmspyder/spyder/management/commands/spyder_all.py
+++ cmspyder/spyder/management/commands/spyder_all.py
@@ -16,7 +16,6 @@ class Command(BaseCommand):
         print("Checking job count")
 
         # get the number of jobs currently running
-        """
         conn = amqp.Connection(host='{0}:{1}'.format(os.environ['RABBIT_MQ_HOST'],
                                                os.environ['RABBIT_MQ_PORT'],),
                                userid='{0}'.format(os.environ['RABBIT_MQ_USER']),
@@ -36,7 +35,6 @@ class Command(BaseCommand):
                 detect_cms.delay(subdomain.id)
         else:
             print("{0} jobs: skipping".format(jobs))
-        """
 
-        for subdomain in Subdomain.objects.order_by('last_scan')[:5000]:
-            detect_cms.delay(subdomain.id)
+        # for subdomain in Subdomain.objects.order_by('last_scan')[:5000]:
+        #     detect_cms.delay(subdomain.id)
